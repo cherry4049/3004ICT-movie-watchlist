febe1f812b29f710eab8d0e5865080db3041d800
cherry chen <cherry4049@hotmail.com>
2026-09-11 23:28:51 +1000

    Set up CineTrack database and models

---
+++ app/Models/Movie.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquen\Relations\BelongsToMany;
+
+class Movie extends Model
+{
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function genres(): BelongToMany
+    {
+        return $this->belongsToMany(Genre::class, 'movie_genre');
+    }
+}
